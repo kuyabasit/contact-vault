05fa3fdcdc29f1dd8eefe2b2ecd89a2db1a22354
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-08-01 18:12:06 +0800

    set and clear current in the provider

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,6 +37,7 @@ const ContactState = (props) => {
         type: 'professional',
       },
     ],
+    current: null,
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -57,10 +58,21 @@ const ContactState = (props) => {
       payload: id,
     });
   };
+
   // Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({
+      type: SET_CURRENT,
+      payload: contact,
+    });
+  };
 
   // Clear Current Contact
-
+  const clearCurrent = () => {
+    dispatch({
+      type: CLEAR_CURRENT,
+    });
+  };
   // Update Contact
 
   // Filter Contacts
@@ -71,8 +83,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
       }}
     >
       {props.children}
