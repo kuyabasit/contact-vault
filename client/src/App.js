81a3a12e84f867aebc39041e2140a82a6e67c45d
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-08-03 21:07:39 +0800

    alert

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,11 @@ import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
@@ -15,19 +17,22 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <div className='App'>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
+        <AlertState>
+          <Router>
+            <div className='App'>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
             </div>
-          </div>
-        </Router>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
